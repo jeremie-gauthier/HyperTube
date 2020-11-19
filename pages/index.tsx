31b56cded0827557803828020d5e15aba09b58bb
Jeremie <jeremiegthr@gmail.com>
2020-11-19 20:23:42 +0100

    remove old Link

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Link from "next/link";
 import styles from "@/styles/Home.module.scss";
 
 export default function Home(): JSX.Element {
@@ -10,9 +9,6 @@ export default function Home(): JSX.Element {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Link href="/movies">
-        <a href="/movies">Movies</a>
-      </Link>
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
