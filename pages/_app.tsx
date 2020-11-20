d64b699ba072400751a707ff6a42a6a9d823602a
Jeremie <jeremiegthr@gmail.com>
2020-11-20 12:21:02 +0100

    put the layout at root level

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import { AppProps } from "next/app";
+import Layout from "@/components/Layout";
 import "@/styles/globals.scss";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
 export default MyApp;
