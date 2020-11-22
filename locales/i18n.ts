3f3649c88ad8b737c12dc2f93ebb7bdb66c75f01
Jeremie <jeremiegthr@gmail.com>
2020-11-22 23:30:06 +0100

    add translations

--- locales/i18n.ts
+++ locales/i18n.ts
@@ -4,10 +4,16 @@ import { initReactI18next } from "react-i18next";
 // This will probably help me later
 // https://stackoverflow.com/questions/58277973/how-to-type-check-i18n-dictionaries-with-typescript
 
+import componentsFR from "./translations/fr/components.json";
+import componentsEN from "./translations/en/components.json";
+import componentsES from "./translations/es/components.json";
+import componentsJA from "./translations/ja/components.json";
+
 const LANGUAGES = {
-  fr: { translation: { pages: {} } },
-  en: { translation: { pages: {} } },
-  es: { translation: { pages: {} } },
+  fr: { translation: { pages: {}, components: componentsFR } },
+  en: { translation: { pages: {}, components: componentsEN } },
+  es: { translation: { pages: {}, components: componentsES } },
+  ja: { translation: { pages: {}, components: componentsJA } },
 };
 
 i18n.use(initReactI18next).init({
