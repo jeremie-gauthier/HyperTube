f4836e16f62ed63c3f82f76d7280b596a5c46640
Jeremie <jeremiegthr@gmail.com>
2020-11-26 11:08:21 +0100

    allow jest to parse next/image

--- tests/jest.setup.ts
+++ tests/jest.setup.ts
@@ -3,3 +3,14 @@ import i18n from "@/locales/i18n.ts";
 
 // setting lng to 'cimode' will set t function to always return the key.
 i18n.changeLanguage("cimode");
+
+process.env = {
+  ...process.env,
+  __NEXT_IMAGE_OPTS: {
+    deviceSizes: [320, 420, 768, 1024, 1200],
+    imageSizes: [],
+    domains: ["images.example.com"],
+    path: "/_next/image",
+    loader: "default",
+  },
+};
