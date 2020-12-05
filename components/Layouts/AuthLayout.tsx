78d6eee8e9b25074f297d523eaed53518e944fa6
Jeremie <jeremiegthr@gmail.com>
2020-12-05 17:07:54 +0100

    add persistent layout

---
+++ components/Layouts/AuthLayout.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import { FlexCol } from "../Flex";
+import styles from "./AuthLayout.module.scss";
+
+type LayoutProps = {
+  children: React.ReactNode;
+};
+
+export default function Layout({ children }: LayoutProps): JSX.Element {
+  return (
+    <FlexCol className={styles.layout} data-testid="layout-div">
+      <Image
+        layout="fill"
+        src="/img/landing.jpg"
+        alt="Movies"
+        priority
+        quality={100}
+      />
+      <div className={styles.brandLogo}>
+        <Image
+          layout="intrinsic"
+          height={54}
+          width={200}
+          src="/icons/hypertube.png"
+          alt="Hypertube logo"
+          priority
+        />
+      </div>
+      <FlexCol className={styles.container}>{children}</FlexCol>
+    </FlexCol>
+  );
+}
