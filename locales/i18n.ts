948a47b5ec5187f271ce124c58097af47b1a70bc
Jeremie <jeremiegthr@gmail.com>
2020-12-05 14:17:41 +0100

    add new translations

--- locales/i18n.ts
+++ locales/i18n.ts
@@ -4,6 +4,11 @@ import { initReactI18next } from "react-i18next";
 // This will probably help me later
 // https://stackoverflow.com/questions/58277973/how-to-type-check-i18n-dictionaries-with-typescript
 
+import pagesFR from "./translations/fr/pages.json";
+import pagesEN from "./translations/en/pages.json";
+import pagesES from "./translations/es/pages.json";
+import pagesJA from "./translations/ja/pages.json";
+
 import componentsFR from "./translations/fr/components.json";
 import componentsEN from "./translations/en/components.json";
 import componentsES from "./translations/es/components.json";
@@ -16,16 +21,16 @@ import commonJA from "./translations/ja/common.json";
 
 const LANGUAGES = {
   fr: {
-    translation: { pages: {}, components: componentsFR, common: commonFR },
+    translation: { pages: pagesFR, components: componentsFR, common: commonFR },
   },
   en: {
-    translation: { pages: {}, components: componentsEN, common: commonEN },
+    translation: { pages: pagesEN, components: componentsEN, common: commonEN },
   },
   es: {
-    translation: { pages: {}, components: componentsES, common: commonES },
+    translation: { pages: pagesES, components: componentsES, common: commonES },
   },
   ja: {
-    translation: { pages: {}, components: componentsJA, common: commonJA },
+    translation: { pages: pagesJA, components: componentsJA, common: commonJA },
   },
 };
 
