2a939c94c85ee08d28e75470b27a2bd2d98e9618
Jeremie <jeremiegthr@gmail.com>
2020-12-06 01:03:49 +0100

    change useform

--- hooks/useForm.tsx
+++ hooks/useForm.tsx
@@ -1,28 +1,32 @@
 import React from "react";
 
-type FormCallback = () => void;
+type FormCallback<V> = (values: V) => void;
 type FormResolver<V> = (values: V) => FormErrors<V>;
-type FormErrors<V> = { [P in keyof V]?: string };
+export type FormErrors<V> = { [P in keyof V]?: string };
 
 type HookForm<V> = {
   values: V;
   errors: FormErrors<V>;
   handleChange: (evt: React.ChangeEvent<HTMLInputElement>) => void;
-  handleSubmit: (evt: React.FormEvent<HTMLButtonElement>) => void;
+  handleSubmit: (evt: React.FormEvent<HTMLFormElement>) => void;
 };
 
 export default function useForm<V>(
-  callback: FormCallback,
+  callback: FormCallback<V>,
   resolver: FormResolver<V>,
   initialValues: V,
 ): HookForm<V> {
   const [values, setValues] = React.useState<V>(initialValues);
   const [errors, setErrors] = React.useState<FormErrors<V>>({});
   const [isSubmitting, setIsSubmitting] = React.useState<boolean>(false);
+  const [hasSubmittingOnce, setHasSubmittingOnce] = React.useState<boolean>(
+    false,
+  );
 
   const handleChange = (evt: React.ChangeEvent<HTMLInputElement>): void => {
     if (values) {
       evt.persist();
+      setIsSubmitting(false);
       setValues((oldValues) => ({
         ...oldValues,
         [evt.target.name]: evt.target.value,
@@ -31,22 +35,23 @@ export default function useForm<V>(
   };
 
   React.useEffect(() => {
-    if (isSubmitting) {
+    if (hasSubmittingOnce) {
       setErrors(resolver(values));
     }
-  }, [resolver, isSubmitting, values]);
+  }, [resolver, hasSubmittingOnce, values]);
 
   const handleSubmit = (evt: React.FormEvent<HTMLFormElement>): void => {
     evt.preventDefault();
     setErrors(resolver(values));
     setIsSubmitting(true);
+    setHasSubmittingOnce(true);
   };
 
   React.useEffect(() => {
     if (isSubmitting && Object.keys(errors).length === 0) {
-      callback();
+      callback(values);
     }
-  }, [callback, isSubmitting, errors]);
+  }, [callback, isSubmitting, errors, values]);
 
   return { values, errors, handleChange, handleSubmit };
 }
