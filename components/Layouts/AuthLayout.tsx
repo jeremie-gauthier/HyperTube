aa23a117827c7e30b3b924cea9b14ceb31a66462
Jeremie <jeremiegthr@gmail.com>
2020-12-19 17:49:45 +0100

    make auth layout responsive with css

--- components/Layouts/AuthLayout.tsx
+++ components/Layouts/AuthLayout.tsx
@@ -1,5 +1,3 @@
-import { useResponsiveAttribute } from "@/hooks/useSelector";
-import Image from "next/image";
 import { FlexCol } from "../Flex";
 import styles from "./AuthLayout.module.scss";
 
@@ -7,51 +5,11 @@ type LayoutProps = {
   children: React.ReactNode;
 };
 
-export default function Layout({ children }: LayoutProps): JSX.Element {
-  const isTabletOrMobile = useResponsiveAttribute();
-
-  return isTabletOrMobile ? (
-    <MobileView>{children}</MobileView>
-  ) : (
-    <DesktopView>{children}</DesktopView>
+export default function Layout({ children }: LayoutProps) {
+  return (
+    <FlexCol className={styles.layout} data-testid="layout-div">
+      <div className={styles.brandLogo} />
+      <FlexCol className={styles.container}>{children}</FlexCol>
+    </FlexCol>
   );
 }
-
-const MobileView = ({ children }: LayoutProps) => (
-  <FlexCol className={styles.layoutMobile}>
-    <div className="mt-4 ml-3">
-      <Image
-        layout="intrinsic"
-        height={32}
-        width={118}
-        src="/icons/hypertube.png"
-        alt="Hypertube logo"
-        priority
-      />
-    </div>
-    {children}
-  </FlexCol>
-);
-
-const DesktopView = ({ children }: LayoutProps) => (
-  <FlexCol className={styles.layoutDesktop} data-testid="layout-div">
-    <Image
-      layout="fill"
-      src="/img/landing.jpg"
-      alt="Movies"
-      priority
-      quality={100}
-    />
-    <div className={styles.brandLogo}>
-      <Image
-        layout="intrinsic"
-        height={54}
-        width={200}
-        src="/icons/hypertube.png"
-        alt="Hypertube logo"
-        priority
-      />
-    </div>
-    <FlexCol className={styles.container}>{children}</FlexCol>
-  </FlexCol>
-);
