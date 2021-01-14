7d2780b92a4dc96b5a1f07c5abc571b858deb6f6
Jeremie <jeremiegthr@gmail.com>
2021-01-14 19:07:00 +0100

    add more data

--- tests/__mocks__/user.ts
+++ tests/__mocks__/user.ts
@@ -1,8 +1,11 @@
+import { TUser } from "@/data/models/User";
+
 export default {
   id: 1,
   firstname: "Albus",
   lastname: "Dumbledore",
   email: "ADumbledore@poudlard.com",
+  username: "GrisPoil",
   password: "@lB|_|5-D0ubl3^DO0R",
   language: "en",
-};
+} as TUser;
