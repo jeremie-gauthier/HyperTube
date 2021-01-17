6be5b80376866c593c7443a0e9d7c6d3dacf5bcd
Jeremie <jeremiegthr@gmail.com>
2021-01-17 18:04:48 +0100

    WIP - account form

--- pages/account/index.tsx
+++ pages/account/index.tsx
@@ -6,18 +6,38 @@ import ScrollBar from "react-perfect-scrollbar";
 import useUser from "@/hooks/useUser";
 import { langs } from "@/locales/i18n";
 import { useRouter } from "next/router";
+import useForm, { HookForm } from "@/hooks/useForm";
+import { AccountForm } from "@/lib/types/account";
+import resolver from "@/lib/resolvers/account";
+import SwitchableInput from "@/components/SwitchableInput";
 import styles from "./account.module.scss";
 
 function Account() {
   const { t } = useTranslation();
+  const { user } = useUser(-42);
+  const initialState = {
+    username: user.username,
+    lastname: user.lastname,
+    firstname: user.firstname,
+    language: user.language,
+  };
+
+  const onSubmit = (values: AccountForm) => {
+    console.log(values);
+  };
+
+  // No ! Must use uncontrolled form instead
+  const methods = useForm<AccountForm>(onSubmit, resolver, initialState);
 
   return (
     <main className={styles.container}>
       <h1 className="title">{t("pages.account.my_account")}</h1>
       <ScrollBar className={styles.scrollContainer}>
-        <SecurityParams />
-        <ProfileParams />
-        <PreferenceParams />
+        <form onSubmit={methods.handleSubmit}>
+          <SecurityParams methods={methods} />
+          <ProfileParams methods={methods} />
+          <PreferenceParams methods={methods} />
+        </form>
       </ScrollBar>
     </main>
   );
@@ -27,13 +47,20 @@ Account.Layout = SiteLayout;
 Account.Title = "pages.account.my_account";
 export default Account;
 
-const SecurityParams = () => {
+type ParamsProps = {
+  methods: HookForm<AccountForm>;
+};
+
+const SecurityParams = ({ methods }: ParamsProps) => {
   const { t } = useTranslation();
   const { user } = useUser(-42);
 
   return (
     <section id="security">
-      <Dropdown title={<h2>{t("pages.account.security.security")}</h2>}>
+      <Dropdown
+        title={<h2>{t("pages.account.security.security")}</h2>}
+        className={styles.dropdown}
+      >
         <Dropdown.Element>
           <div className="font-semibold">{user.email}</div>
           <Link href="account/change/email">
@@ -41,10 +68,19 @@ const SecurityParams = () => {
           </Link>
         </Dropdown.Element>
         <Dropdown.Element>
-          <div>{user.username}</div>
-          <Link href="account/change/username">
-            {t("pages.account.security.edit_username")}
-          </Link>
+          <SwitchableInput>
+            <SwitchableInput.Input
+              className={styles.switchableInput}
+              name="username"
+              placeholder={t("models.user.username")}
+              methods={methods}
+            />
+            <SwitchableInput.Toggle
+              label={t("pages.account.security.edit_username")}
+              isLoading={false}
+              className="flex flex-row items-center space-x-4"
+            />
+          </SwitchableInput>
         </Dropdown.Element>
         <Dropdown.Element>
           <div>{"*".repeat(8)}</div>
@@ -57,13 +93,16 @@ const SecurityParams = () => {
   );
 };
 
-const ProfileParams = () => {
+const ProfileParams = ({ methods }: ParamsProps) => {
   const { t } = useTranslation();
   const { user } = useUser(-42);
 
   return (
     <section id="profile">
-      <Dropdown title={<h2>{t("pages.account.profile.my_profile")}</h2>}>
+      <Dropdown
+        title={<h2>{t("pages.account.profile.my_profile")}</h2>}
+        className={styles.dropdown}
+      >
         <Dropdown.Element>
           <div>{user.lastname}</div>
           <button type="button">
@@ -87,7 +126,7 @@ const ProfileParams = () => {
   );
 };
 
-const PreferenceParams = () => {
+const PreferenceParams = ({ methods }: ParamsProps) => {
   const { t } = useTranslation();
   const { user } = useUser(-42);
   const { asPath } = useRouter();
@@ -97,6 +136,7 @@ const PreferenceParams = () => {
       <Dropdown
         title={<h2>{t("pages.account.preferences.preferences")}</h2>}
         initialState={asPath.includes("#preferences")}
+        className={styles.dropdown}
       >
         <Dropdown.Element>
           <div>{t(langs[user.language])}</div>
