657ec778680cc2bc5be243097f6ae36773d6a0e8
Jeremie <jeremiegthr@gmail.com>
2021-01-22 14:47:01 +0100

    add forwardRef on FormInput

--- components/FormInput/index.tsx
+++ components/FormInput/index.tsx
@@ -13,49 +13,51 @@ type DefaultProps = React.InputHTMLAttributes<HTMLInputElement> & {
   error: string;
 };
 
-export default function FormInput({
-  error = undefined,
-  ...rest
-}: FormInputProps) {
-  const { hasFocus, handleFocus, handleBlur } = useFocus(
-    rest.onFocus,
-    rest.onBlur,
-  );
-  const hasValue = rest.value.length > 0;
-  const showLabel = hasFocus || hasValue;
-  const { t } = useTranslation();
+const FormInput = React.forwardRef<HTMLInputElement, FormInputProps>(
+  ({ error = undefined, ...rest }, forwardRef) => {
+    const { hasFocus, handleFocus, handleBlur } = useFocus(
+      rest.onFocus,
+      rest.onBlur,
+    );
+    const hasValue = rest.value.length > 0;
+    const showLabel = hasFocus || hasValue;
+    const { t } = useTranslation();
 
-  const isFocused = classnames({
-    [rest.className ?? ""]: true,
-    "border-white": hasFocus,
-    "border-grey-light": !hasFocus,
-    [styles.inputError]: error,
-  });
-  const labelStyle = classnames({
-    "text-white": hasFocus,
-    "text-grey-light": !hasFocus,
-  });
+    const isFocused = classnames({
+      [rest.className ?? ""]: true,
+      "border-white": hasFocus,
+      "border-grey-light": !hasFocus,
+      [styles.inputError]: error,
+    });
+    const labelStyle = classnames({
+      "text-white": hasFocus,
+      "text-grey-light": !hasFocus,
+    });
 
-  return (
-    <div className={styles.container}>
-      {showLabel && (
-        <label htmlFor={rest.name} className={labelStyle}>
-          {rest.placeholder}
-        </label>
-      )}
-      <input
-        id={rest.name}
-        {...rest}
-        className={isFocused}
-        placeholder={showLabel ? "" : rest.placeholder}
-        onBlur={handleBlur}
-        onFocus={handleFocus}
-        autoComplete="off"
-      />
-      {error && <p>{t(error)}</p>}
-    </div>
-  );
-}
+    return (
+      <div className={styles.container}>
+        {showLabel && (
+          <label htmlFor={rest.name} className={labelStyle}>
+            {rest.placeholder}
+          </label>
+        )}
+        <input
+          ref={forwardRef}
+          id={rest.name}
+          {...rest}
+          className={isFocused}
+          placeholder={showLabel ? "" : rest.placeholder}
+          onBlur={handleBlur}
+          onFocus={handleFocus}
+          autoComplete="off"
+        />
+        {error && <p>{t(error)}</p>}
+      </div>
+    );
+  },
+);
+
+export default FormInput;
 
 type FocusEvent = (evt: React.FocusEvent<HTMLInputElement>) => void;
 
