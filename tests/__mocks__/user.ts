3f105f83bc4b0d2f41b78d69cf774846073a2323
Jeremie <jeremiegthr@gmail.com>
2021-01-23 12:07:31 +0100

    fix old mock

--- tests/__mocks__/user.ts
+++ tests/__mocks__/user.ts
@@ -1,11 +1,12 @@
-import { TUser } from "@/data/models/User";
+import { User } from "@/types/user";
 
 export default {
-  id: 1,
+  id: "1",
+  username: "GrisPoil",
   firstname: "Albus",
   lastname: "Dumbledore",
   email: "ADumbledore@poudlard.com",
-  username: "GrisPoil",
   password: "@lB|_|5-D0ubl3^DO0R",
+  picture: 1,
   language: "en",
-} as TUser;
+} as User;
