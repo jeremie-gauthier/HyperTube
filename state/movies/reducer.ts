af3befb9f7c7c379d76c58f9dbf4a24ab00cf6e9
Jeremie <jeremiegthr@gmail.com>
2021-03-25 17:08:45 +0100

    add state logic to change categ

--- state/movies/reducer.ts
+++ state/movies/reducer.ts
@@ -1,9 +1,10 @@
 import { MovieActions, MovieData } from "./types";
 
-import { SET_SEARCH_INPUT } from "./actions";
+import { SET_SEARCH_INPUT, SET_SELECTED_CATEGORY } from "./actions";
 
 export const initialState: MovieData = {
   searchInput: "",
+  selectedCategory: null,
 };
 
 export default function reducer(
@@ -13,6 +14,8 @@ export default function reducer(
   switch (action.type) {
     case SET_SEARCH_INPUT:
       return { ...state, searchInput: action.payload.searchInput };
+    case SET_SELECTED_CATEGORY:
+      return { ...state, selectedCategory: action.payload.selectedCategory };
     default:
       return state;
   }
--- state/movies/types.ts
+++ state/movies/types.ts
@@ -1,12 +1,20 @@
-export type MovieActions = ActionSetSearchInput;
+export type MovieActions = ActionSetSearchInput | ActionSetSelectedCategory;
 
 export type MovieData = {
   searchInput: string;
+  selectedCategory: string | null;
 };
 
 export type ActionSetSearchInput = {
   type: "SET_SEARCH_INPUT";
   payload: {
     searchInput: string;
+  };
+};
+
+export type ActionSetSelectedCategory = {
+  type: "SET_SELECTED_CATEGORY";
+  payload: {
+    selectedCategory: string | null;
   };
 };
